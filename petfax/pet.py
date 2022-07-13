2cefb15f6f3c5a966bc36ebf89fe3369a2f5642e
Kaleab007 <abatenehkaleab@gmail.com>
2022-07-13 00:00:37 -0700

    index

--- petfax/pet.py
+++ petfax/pet.py
@@ -1,5 +1,9 @@
 from flask import (Blueprint , render_template)
+import json
+
+pets = json.load(open('pets.json')
+print(pets))
 
 @bp.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', pets=pets)
